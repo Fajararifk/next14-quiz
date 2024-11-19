fd5e3b89216ee89f526a4bf283851ab4e0f37d4c
Fajar Arif Kurniawan <fajar.kurniawan@amartek.id>
2024-11-20 05:04:45 +0700

    edit middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/"], // Mark the `/` route as public
-});
+export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
